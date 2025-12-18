5c921566b8835b36eeaaa32661e1d22f45676a72
RubenCode <58192099+ProCodeMundo@users.noreply.github.com>
2025-12-18 18:07:46 -0500

    Update streamlit_app.py
    Connected changes needed bc of move form SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,6 +10,9 @@ st.write(
   """
 )
 
+# Connecting to github 
+cnx =st.connection("snowflake")
+session = cnx.session()
 
 #Adding a Name Box for Smoothie Orders
 
