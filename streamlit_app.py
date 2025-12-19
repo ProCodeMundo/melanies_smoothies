85bf1717fab3c6982c2c76fa3806e20001b97e79
RubenCode <58192099+ProCodeMundo@users.noreply.github.com>
2025-12-19 04:46:39 -0500

    Update streamlit_app.py
    Moving code to the end:
    # New Section to display smoothiefroot nutrition information 
    import requests
    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
    st.text(smoothiefroot_response)

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-# New Section to display smoothiefroot nutrition information 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
 
 
 # Write directly to the app
@@ -66,5 +62,11 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered'+ ' ' +name_on_order+ '!', icon="✅")
+
+
+# New Section to display smoothiefroot nutrition information 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
   
