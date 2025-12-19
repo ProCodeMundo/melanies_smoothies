a7a7b52af190abe698945060cf8e50c673913268
RubenCode <58192099+ProCodeMundo@users.noreply.github.com>
2025-12-19 04:43:28 -0500

    Update streamlit_app.py
    Adding Comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+# New Section to display smoothiefroot nutrition information 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
